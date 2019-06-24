767db719b5e0d559ddea9b42774f14351ad6d684
czoido <mrgalleta@gmail.com>
2019-06-24 10:52:17 +0200

    change triangle color

--- main.cpp
+++ main.cpp
@@ -146,15 +146,22 @@ int main(int, char **)
 		glBindVertexArray(0);
 
 		// render your GUI
-		ImGui::Begin("Triangle Position");
+		ImGui::Begin("Triangle Position/Color");
 		static float rotation = 0.0;
 		ImGui::SliderFloat("rotation", &rotation, 0, 2 * PI);
 		static float translation[] = {0.0, 0.0};
 		ImGui::SliderFloat2("position", translation, -1.0, 1.0);
-		ImGui::End();
-		// pass the parameters to the shader
-		triangle_shader.setUniform("rotation", rotation);
-		triangle_shader.setUniform("translation", translation[0], translation[1]);
+        static float color[4] = { 1.0f,1.0f,1.0f,1.0f };
+        // pass the parameters to the shader
+        triangle_shader.setUniform("rotation", rotation);
+        triangle_shader.setUniform("translation", translation[0], translation[1]);
+    
+        // color picker
+        ImGui::ColorEdit3("color", color);
+        // multiply triangle's color with this color
+        triangle_shader.setUniform("color", color[0], color[1], color[2]);
+        
+        ImGui::End();
 
 		// Render dear imgui into screen
 		ImGui::Render();
